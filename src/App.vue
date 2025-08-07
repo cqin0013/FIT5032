829df0547c1f17e32ebdc179889075edd4e514c5
cqin0013 <cqin0013@student.monash.edu>
2025-08-07 19:51:44 +1000

    week1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MyName from './MyName.vue';
+</script>
+
+
+<template>
+  <h1>You did ittttt!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+  <MyName />
+</template>
+
+<style scoped></style>
---
+++ src/MyName.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Hello, my name is Chenjin Qin!</h2>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h2 {
+  color: teal;
+}
+</style>
