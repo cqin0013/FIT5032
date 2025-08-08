5781e75bb84e83d13f056dfb3ee2dcab1bf5e739
cqin0013 <cqin0013@student.monash.edu>
2025-08-08 23:02:44 +1000

    week2

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- App.vue -->
+<script setup>
+import JSONVue from './components/JSON.vue'
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+
+  <JSONVue />
+</template>
+
+<style scoped></style>
