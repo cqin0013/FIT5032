18e0acefb11865d4ab636912e7db0f9b79fa46ef
cqin0013 <cqin0013@student.monash.edu>
2025-08-24 14:59:00 +1000

    week4

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Bootstrap（如果你在用）
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-// PrimeVue 主题与组件（注意 @primeuix/themes）
+// PrimeVue
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import 'primeicons/primeicons.css'
 
-// DataTable 组件
+// DataTable
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 
