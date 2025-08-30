3b2bc255b667e6e7d9960db5c26bd3a63d3ad12e
cqin0013 <cqin0013@student.monash.edu>
2025-08-30 20:08:56 +1000

    a1

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<!-- App.vue -->
+<script setup>
+import JSONVue from './components/JSON.vue'
+import Form from "./components/Form.vue";
+</script>
+
+<template>
+
+
+
+  <!--<JSONVue />-->
+  <Form />
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,32 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// PrimeVue
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeicons/primeicons.css'
+
+// DataTable
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+
+app.mount('#app')
+
+if (process.env.NODE_ENV === 'production') {
+  Vue.config.devtools = false;
+}
